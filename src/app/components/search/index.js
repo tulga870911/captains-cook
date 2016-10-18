27714fefa4d09eccb633fb5a876d2da3c309b1da
tulga870911 <tulga870911@gmail.com>
2016-10-18 22:36:39 +0200

    feat(search): add icons for search form

--- src/app/components/search/index.js
+++ src/app/components/search/index.js
@@ -15,7 +15,7 @@ function SearchCtrl($log, $q, $timeout, $state, $rootScope) {
     init();
   }
 
-  $rootScope.$on('$destroy', $rootScope.$on('LOCALITY_UPDATED', function(){
+  $rootScope.$on('$destroy', $rootScope.$on('LOCALITY_UPDATED', function () {
     vm.locality = $rootScope.locality;
   }));
 
@@ -32,5 +32,62 @@ function SearchCtrl($log, $q, $timeout, $state, $rootScope) {
     $rootScope.$emit('SHOW_SEARCH_RESULTS');
 
     $state.go('main.results');
+  }
+
+  // second selector
+
+  vm.simulateQuery = false;
+  vm.isDisabled = false;
+
+  vm.repos = loadAll();
+  vm.querySearch = querySearch;
+  vm.selectedItemChange = selectedItemChange;
+  vm.searchTextChange = searchTextChange;
+
+  function querySearch(query) {
+    var results = query ? vm.repos.filter(createFilterFor(query)) : vm.repos,
+      deferred;
+    if (vm.simulateQuery) {
+      deferred = $q.defer();
+      $timeout(function () { deferred.resolve(results); }, Math.random() * 1000, false);
+      return deferred.promise;
+    } else {
+      return results;
+    }
+  }
+
+  function searchTextChange(text) {
+    $log.info('Text changed to ' + text);
+  }
+
+  function selectedItemChange(item) {
+    $log.info('Item changed to ' + JSON.stringify(item));
+  }
+
+  function loadAll() {
+    var repos = [{
+      'name': '111',
+      'url': '111',
+      'watchers': '11',
+      'forks': '11',
+    }, {
+      'name': '11',
+      'url': '11',
+      'watchers': '469',
+      'forks': '760',
+    }];
+    return repos.map(function (repo) {
+      repo.value = repo.name.toLowerCase();
+      return repo;
+    });
+  }
+
+  function createFilterFor(query) {
+    var lowercaseQuery = angular.lowercase(query);
+
+    return function filterFn(item) {
+      return (item.value.indexOf(lowercaseQuery) === 0);
+    };
+
   }
 }
